7d9b0cf72478356fdb4579355484a16938307978
philmaxsnr <philmaxsnr@gmail.com>
2020-05-16 13:16:52 +0300

    update

--- controllers/user.js
+++ controllers/user.js
@@ -61,7 +61,7 @@ function loginUser(req, res) {
                         });
                     } else {
                         user.password = undefined;
-                        user.token = jwt.createtoken(user);
+                        user.token = "token";
                         console.log(jwt.createtoken(user))
                         return res.status(200).send({user});
                     }
