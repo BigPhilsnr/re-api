6f3768532a181e65d048711ffe5c09c9c46b9cba
philmaxsnr <philmaxsnr@gmail.com>
2020-05-16 12:02:41 +0300

    update

--- routes/default.js
+++ routes/default.js
@@ -5,7 +5,7 @@ var api = express.Router();
 var DefaultController = require('../controllers/default');
 
 api.get('/', DefaultController.help);
-api.get('/status', DefaultController.status);
+api.post('/status', DefaultController.status);
 api.get( '*',DefaultController.notFound)
 
 module.exports = api;
