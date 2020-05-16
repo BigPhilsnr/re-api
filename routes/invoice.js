e588d16ba74e30fc13d2609df24d3731c99bfab9
philmaxsnr <philmaxsnr@gmail.com>
2020-05-16 21:52:08 +0300

    update

--- routes/invoice.js
+++ routes/invoice.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var express = require('express');
-var Invoicecontroller = require('../controllers/invoice');
+var Invoicecontroller = require('../controllers/invoie');
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 api.post('/invoice', md_auth.ensureAuth, Invoicecontroller.createInvoice);
