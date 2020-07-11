92287b84490ab9b7eca971f82ac482730fac6a50
ajuxx <ajusabuyu@gmail.com>
2020-07-11 12:18:02 +0300

    adds route on app.js

--- app.js
+++ app.js
@@ -8,53 +8,64 @@ var user_routes = require('./routes/user');
 var follow_routes = require('./routes/follow');
 var message_routes = require('./routes/message');
 var publication_routes = require('./routes/publication');
-const sellerRoute = require('./routes/seller')
-const shopRoute = require('./routes/shop')
-const itemRoute = require('./routes/item')
-const orderRoute = require('./routes/order')
-const agentRoute = require('./routes/agent')
-const expenseRoute = require('./routes/expense')
-const paymentRoute = require('./routes/payment')
-const housingUnitRoute = require('./routes/housingunit')
+const sellerRoute = require('./routes/seller');
+const shopRoute = require('./routes/shop');
+const itemRoute = require('./routes/item');
+const orderRoute = require('./routes/order');
+const agentRoute = require('./routes/agent');
+const expenseRoute = require('./routes/expense');
+const paymentRoute = require('./routes/payment');
+const housingUnitRoute = require('./routes/housingunit');
 const propertyRoute = require('./routes/property');
 const invoiceRoute = require('./routes/invoice');
-const employeeRoute = require('./routes/employee')
-const adminRoute  = require('./routes/admin')
-const tenantRoute = require('./routes/tenant')
-const morgan = require('morgan')
+const employeeRoute = require('./routes/employee');
+const adminRoute = require('./routes/admin');
+const tenantRoute = require('./routes/tenant');
+const mailRoute = require('./routes/mail');
+
+const morgan = require('morgan');
 const exec = require('child_process').exec;
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: false,
+	})
+);
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Expose-Headers','Authorization')
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Expose-Headers', 'Authorization');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+	);
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
 });
 
-app.use(morgan(function (tokens, req, res) {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms'
-    ].join(' ')
-}))
+app.use(
+	morgan(function (tokens, req, res) {
+		return [
+			tokens.method(req, res),
+			tokens.url(req, res),
+			tokens.status(req, res),
+			tokens.res(req, res, 'content-length'),
+			'-',
+			tokens['response-time'](req, res),
+			'ms',
+		].join(' ');
+	})
+);
 
-app.post("/status", async (req, res) => {
-    console.log("updating app from github inde")
-    exec('sh social.sh');
-    res.status(200).send({
-        ok: 1
-    });
-})
+app.post('/status', async (req, res) => {
+	console.log('updating app from github inde');
+	exec('sh social.sh');
+	res.status(200).send({
+		ok: 1,
+	});
+});
 
 app.use('/api', user_routes);
 app.use('/api', follow_routes);
@@ -63,7 +74,7 @@ app.use('/api', publication_routes);
 app.use('/api', sellerRoute);
 app.use('/api', shopRoute);
 app.use('/api', itemRoute);
-app.use('/api', orderRoute)
+app.use('/api', orderRoute);
 app.use('/api', agentRoute);
 app.use('/api', expenseRoute);
 app.use('/api', paymentRoute);
@@ -71,8 +82,10 @@ app.use('/api', housingUnitRoute);
 app.use('/api', propertyRoute);
 app.use('/api', invoiceRoute);
 app.use('/api', employeeRoute);
-app.use('/api', adminRoute)
-app.use('/api/',tenantRoute)
+app.use('/api', adminRoute);
+app.use('/api', mailRoute);
+
+app.use('/api/', tenantRoute);
 app.use('/images', express.static(__dirname + '/uploads'));
 app.use('/', default_routes);
 
--- routes/mail.js
+++ routes/mail.js
@@ -5,7 +5,7 @@ var Mailcontroller = require('../controllers/mail');
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
-api.post('/admin', md_auth.ensureAuth, Mailcontroller.createAdmin);
-api.get('/admin', md_auth.ensureAuth, Mailcontroller.getAdmin);
+api.post('/mail', md_auth.ensureAuth, Mailcontroller.createAdmin);
+api.get('/mail', md_auth.ensureAuth, Mailcontroller.getAdmin);
 
 module.exports = api;
