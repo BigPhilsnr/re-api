2247a926553e8c718a68da423d51d5fc7c290482
philmaxsnr <philmaxsnr@gmail.com>
2021-07-02 23:27:53 +0300

    changed port

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 
 const mongoose = require('mongoose');
 const app = require('./app');
-const port = process.env.PORT || 3800;
-const mongoDb = process.env.MONGODB_URL || 'mongodb://localhost:27017/social';
+const port = process.env.PORT || 9000;
+const onlineDb =  "mongodb+srv://heroku:iznF7iVVyq4JJ01c@cluster0.cwv6b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const mongoDb = process.env.MONGODB_URL ||  onlineDb ||'mongodb://localhost:27017/social';
 
 mongoose.connect(mongoDb, { useNewUrlParser: true })
 .then(() => {
